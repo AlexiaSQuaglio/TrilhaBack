a9c033e8cb44380aafdf8305b570feddda6dfba5
Alexia Sanchez Quaglio <95059002+AlexiaSQuaglio@users.noreply.github.com>
2021-12-17 12:31:25 -0300

    :wastebasket: retirando comentarios

--- src/main/java/trilha/back/financys/FinancysApplication.java
+++ src/main/java/trilha/back/financys/FinancysApplication.java
@@ -2,22 +2,12 @@ package trilha.back.financys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.stereotype.Component;
-
-//@EntityScan(basePackages = ("trilha.back.financys.entities")) //onde que está nossas entidades
-//@ComponentScan(basePackages = ("controller"))
-//@Component
 
 @SpringBootApplication
-
 public class FinancysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancysApplication.class, args);
-		
 	}
-
 	
 }
