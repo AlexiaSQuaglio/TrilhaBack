13c36dc700840f3071d645aff070f2282d62f0e1
Alexia Sanchez Quaglio <95059002+AlexiaSQuaglio@users.noreply.github.com>
2022-01-03 17:34:26 -0300

    desafio7

---
+++ src/main/java/trilha/back/financys/dto/ChartDTO.java
@@ -0,0 +1,13 @@
+package trilha.back.financys.dto;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChartDTO {
+    private String name;
+    private double amount;
+}
